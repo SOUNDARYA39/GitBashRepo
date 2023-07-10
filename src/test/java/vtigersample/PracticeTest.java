a4be367c88cd5bee73b7341a2d1d715f47294117
SOUNDARYA39 <soundaryagpt.g2@gmail.com>
2023-07-10 11:47:37 +0530

    firstcommit

---
+++ src/test/java/vtigersample/PracticeTest.java
@@ -0,0 +1,29 @@
+package vtigersample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+
+public class PracticeTest {
+	WebDriver driver;
+	@Test
+	
+	public void m1()
+	{
+		
+		String Browser=System.getProperty("browser");
+		String Url = System.getProperty("url");
+		
+		if (Browser.equalsIgnoreCase("chrome"))
+		{
+			driver=new ChromeDriver();
+		}
+		else
+		{
+			driver=new FirefoxDriver();
+		}
+		driver.get(Url);
+	}
+}
